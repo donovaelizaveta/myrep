938ed93f9bae7a53c77b2596cfb351be427a94be
donovaelizaveta <donovaelizaveta@gmail.com>
2018-10-21 14:10:42 +0300

    добавление файла C#

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Example
+    {
+        static void Main()
+        {
+            Console.WriteLine("Привет! Это - моя первая программа на C#");
+            Console.WriteLine("Нажмите любую кнопку!");
+            Console.ReadKey();
+        }
+    }
+}
